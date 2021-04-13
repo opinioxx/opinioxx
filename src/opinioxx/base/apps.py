7bec77208be9c0412725acfc04d85ce7494f3946
Chessmaster <Chessmasterrr@users.noreply.github.com>
2021-04-13 08:29:55 +0200

    update dependencies

--- src/opinioxx/base/apps.py
+++ src/opinioxx/base/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class OpinioxxBaseConfig(AppConfig):
     name = 'opinioxx.base'
     verbose_name = 'Opinioxx Base'
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         from . import signals
